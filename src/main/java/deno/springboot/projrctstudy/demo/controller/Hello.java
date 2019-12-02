75ab63133baa8461fa28def48de8dce4a9c21cf1
zjz <zjzgrh@163.com>
2019-12-02 14:19:52 +0800

    first commit

---
+++ src/main/java/deno/springboot/projrctstudy/demo/controller/Hello.java
@@ -0,0 +1,15 @@
+package deno.springboot.projrctstudy.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Hello {
+    @GetMapping("/hello")
+    public String getName(@RequestParam(name = "name") String name,Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
